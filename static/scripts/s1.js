4cc0bdb1696f6df16d0a7a9005367f321ba0163a
RushabhM03 <rushabh.maru123@gmail.com>
2023-06-23 12:18:20 +0530

    loading and dark-mode bugs fixed

--- static/scripts/s1.js
+++ static/scripts/s1.js
@@ -2,8 +2,12 @@
 const modeToggle = document.getElementById('toggle-mode');
 
 document.addEventListener('DOMContentLoaded', function() {
+    
+    const body = document.body;
+    const navbar = document.getElementsByTagName('nav');
+
     var storedMode = localStorage.getItem('mode');
-    var body = document.body;
+    body.classList.remove('trans');
 
     if (storedMode === 'light-mode') {
         body.classList.remove('dark-mode');
@@ -14,12 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.add('dark-mode');
         modeToggle.innerHTML = '<?xml version="1.0" ?><svg class="-mr-8" id="Layer_1_1_" style="enable-background:new 0 0 16 16;" height="36px" width="36px" fill="white" version="1.1" viewBox="0 0 16 16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="8.5" cy="7.5" r="4.5"/><rect height="2" width="1" x="8"/><rect height="2" width="1" x="8" y="13"/><rect height="1" width="2" x="14" y="7"/><rect height="1" width="2" x="1" y="7"/><rect height="2" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -4.7175 12.8033)" width="1" x="12.596" y="11.096"/><rect height="2" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -0.9099 3.6109)" width="1" x="3.404" y="1.904"/><rect height="1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -7.4099 6.3033)" width="2" x="2.904" y="11.596"/><rect height="1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 1.7823 10.1107)" width="2" x="12.096" y="2.404"/></svg>'
     }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const body = document.body;
-    const navbar = document.getElementsByTagName('nav');
   
     modeToggle.addEventListener('click', function() {
       //body.classList.toggle('dark-mode');
@@ -28,12 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.add('dark-mode')
         //sessionStorage.setItem('mode', 'dark-mode')
         localStorage.setItem('mode', 'dark-mode');
+        body.classList.add('trans');
         
       }else if(body.classList.contains('dark-mode')){
         body.classList.remove('dark-mode')
         body.classList.add('light-mode')
         //sessionStorage.setItem('mode', 'light-mode')
         localStorage.setItem('mode', 'light-mode');
+        body.classList.add('trans');
       }
     });
   });
